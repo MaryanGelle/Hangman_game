67907983d9269d47f1a8fe9945c70da470953225
MaryanGelle <>
2023-07-13 15:45:48 +0000

    changes

--- run.py
+++ run.py
@@ -3,39 +3,23 @@ import random
 
 print("Welcome to Hangman Game")
 
-wordChoice = ("noise", "ears", "skin", "5", "25", "egypt", "up", "oxygen", "food", "summer")
-chosen_word = random.choice(wordChoice)
-print(chosen_word)
-
-display = ""
-for letter in chosen_word:
-    display += '_'
-
-
-print(display)
-guessed_letter=input("guess a letter: ")
-
-
-# choose a random word
-
-
 
 def print_hangman(guesses):
-    if guesses == 0:
+    if guesses == 6:
         print("________    ")
         print("|      |    ")
         print("|           ")
         print("|           ")
         print("|           ")
         print("|           ")
-    elif guesses == 1:
+    elif guesses == 5:
         print("________    ")
         print("|      |    ")
         print("|      0    ")
         print("|           ")
         print("|           ")
         print("|           ")
-    elif guesses == 2:
+    elif guesses == 4:
         print("________    ")
         print("|      |    ")
         print("|      0    ")
@@ -49,14 +33,14 @@ def print_hangman(guesses):
         print("|     /|    ")
         print("|           ")
         print("|           ")
-    elif guesses == 4:
+    elif guesses == 2:
         print("________    ")
         print("|      |    ")
         print("|      0    ")
         print("|     /|\   ")
         print("|           ")
         print("|           ")
-    elif guesses == 5:
+    elif guesses == 1:
         print("________    ")
         print("|      |    ")
         print("|      0    ")
@@ -73,3 +57,60 @@ def print_hangman(guesses):
         print("Wrong answer hanged the man")
         print("Game over!")
 
+
+def hangman():
+    # List of words to choose from 
+    wordChoice = ["noise", "ears", "skin", "5", "25", "egypt", "up", "oxygen", "food", "summer"]
+
+    # Select a random word from the list 
+    chosen_word = random.choice(wordChoice)
+
+    # Game state
+    guesses = 6
+    guessed_letters = []
+
+    while guesses > 0:
+        # Print hangman
+        print_hangman(guesses)
+
+        # Print the currently guessed word with underscores for missing letters
+        display_word = ""
+        for letter in chosen_word:
+            if letter in guessed_letters:
+                display_word += letter
+            else:
+                display_word += "_"
+        print(display_word)
+
+        # Prompt the user for a letter guess
+        guess = input("Guess a letter: ")
+        
+        # Check if the input is a valid single letter
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input! Please enter a single letter.")
+            continue
+        
+        # Check if the letter has already been guessed
+        if guess in guessed_letters:
+            print("You've already guessed that letter. Try again.")
+            continue
+        
+        # Check if the guessed letter is correct
+        if guess in chosen_word:
+            print("Correct guess!")
+            guessed_letters.append(guess)
+        else:
+            print("Wrong guess!")
+            guesses -= 1
+        
+        # Check if the player has won
+        if all(letter in guessed_letters for letter in chosen_word):
+            print("Congratulations! You won! The word was correct")
+            break
+    
+    # If the player ran out of guesses, they lose the game
+    if guesses == 0:
+        print("Sorry, you lost the game.")
+
+
+hangman()
